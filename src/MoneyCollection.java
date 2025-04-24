a5ddf13ea51de9df1996ccfd3213ae26733c1475
samkits <samkitsalot0@gmail.com>
2025-04-24 15:19:52 +0530

    Added Money Enter state class.

---
+++ src/MoneyCollection.java
@@ -0,0 +1,33 @@
+public class MoneyCollection implements UpdateState  {
+
+    private Integer remainingMoney;
+
+    public MoneyCollection(Integer remainingMoney){
+        this.remainingMoney = remainingMoney;
+    }
+
+    public void dispatchMoney(State state){
+        if(this.isStateValid(state)){
+            if(this.remainingMoney > 0){
+                System.out.println("Collect the remaining Money: " + this.remainingMoney);
+            }
+            this.next(state);
+        }
+    }
+
+    @Override
+    public void next(State state){
+        if(this.isStateValid(state)){
+            state.setCurrentState(StateType.IDEAL);
+        }
+    }
+
+    @Override
+    public Boolean isStateValid(State state) {
+        if(state.getCurrentState() != StateType.MONEY_COLLECTION){
+            this.throwAccessDeniedError();
+            return false;
+        }
+        return true;
+    }
+}
